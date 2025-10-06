dbf41759d75c851afb05804c4b07be4234d04551
thanadate888 <thanadateee@gmail.com>
2025-10-06 08:24:48 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Numer/', // 👈 ใส่ชื่อ repo ตรงนี้ (ถ้า repo เธอชื่อ Numer)
   plugins: [react()],
 })
+
